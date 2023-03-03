f6ee6325719dc8a80f4fd58c1a3262cb5ff27520
bonf <apwierk2451@gmail.com>
2023-03-03 18:50:20 +0900

    docs: GifTogether 파일 생성

---
+++ GifTogether/GifTogether/GifTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GifTogetherApp.swift
+//  GifTogether
+//
+//  Created by 이은찬 on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct GifTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
