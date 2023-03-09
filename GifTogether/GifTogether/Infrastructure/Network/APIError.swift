6887158a9c150d4b42814ce97243eecc5befe3f4
wongbingg <wb277@naver.com>
2023-03-09 17:44:28 +0900

    feat : APIError 정의 #4
    - APIClient 에서 발생하는 Error 타입 정의

---
+++ GifTogether/GifTogether/Infrastructure/Network/APIError.swift
@@ -0,0 +1,22 @@
+//
+//  APIError.swift
+//  GifTogether
+//
+//  Created by 이원빈 on 2023/03/09.
+//
+
+import Foundation
+
+enum APIError: LocalizedError {
+    case unknown
+    case statusCode(Int)
+    
+    var errorDescription: String? {
+        switch self {
+        case .unknown:
+            return "알 수 없는 네트워크 오류 발생"
+        case .statusCode(let code):
+            return "현재 statucCode: \(code) 오류발생"
+        }
+    }
+}
