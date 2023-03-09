7db9757db1e60db3908bef541298ef55ed0424da
wongbingg <wb277@naver.com>
2023-03-09 19:42:45 +0900

    refactor : APIError에 badURLRequest 추가 #4

--- GifTogether/GifTogether/Infrastructure/Network/APIError.swift
+++ GifTogether/GifTogether/Infrastructure/Network/APIError.swift
@@ -10,6 +10,7 @@ import Foundation
 enum APIError: LocalizedError {
     case unknown
     case statusCode(Int)
+    case badURLRequest
     
     var errorDescription: String? {
         switch self {
@@ -17,6 +18,8 @@ enum APIError: LocalizedError {
             return "알 수 없는 네트워크 오류 발생"
         case .statusCode(let code):
             return "현재 statucCode: \(code) 오류발생"
+        case .badURLRequest:
+            return "URLRequest가 잘못 되었습니다."
         }
     }
 }
