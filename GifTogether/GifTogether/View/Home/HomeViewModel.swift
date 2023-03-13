296c08ebd5886d3e330268714c0653b403aadb96
bonf <apwierk2451@gmail.com>
2023-03-13 16:37:05 +0900

    refactor : load 전역 함수를 JSONManager 타입 메소드로 생성 #5

--- GifTogether/GifTogether/View/Home/HomeViewModel.swift
+++ GifTogether/GifTogether/View/Home/HomeViewModel.swift
@@ -8,28 +8,31 @@
 import SwiftUI
 
 final class HomeViewModel: ObservableObject {
-    var categoriesInfo: [Category] = load("categories.json")
-    @Published var gifticonsInfo: [Gifticon] = load("gifticons.json")
+    var categoriesInfo: [Category] = JSONManager.load("categories.json")
+    @Published var gifticonsInfo: [Gifticon] = JSONManager.load("gifticons.json")
 }
 
-func load<T: Decodable>(_ filename: String) -> T {
-    let data: Data
 
-    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+final class JSONManager {
+    static func load<T: Decodable>(_ filename: String) -> T {
+        let data: Data
+        
+        guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
         else {
             fatalError("Couldn't find \(filename) in main bundle.")
-    }
-
-    do {
-        data = try Data(contentsOf: file)
-    } catch {
-        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
-    }
-
-    do {
-        let decoder = JSONDecoder()
-        return try decoder.decode(T.self, from: data)
-    } catch {
-        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+        }
+        
+        do {
+            data = try Data(contentsOf: file)
+        } catch {
+            fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+        }
     }
 }
