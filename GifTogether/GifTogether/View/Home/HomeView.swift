6634d9a33f38847061214cb27dbad9be5bcf98a6
bonf <apwierk2451@gmail.com>
2023-03-13 18:18:43 +0900

    style : BannerView로 이름 변경

--- GifTogether/GifTogether/View/Home/HomeView.swift
+++ GifTogether/GifTogether/View/Home/HomeView.swift
@@ -13,7 +13,7 @@ struct HomeView: View {
     var body: some View {
         NavigationView {
             List {
-                PageView()
+                BannerView()
                     .listRowInsets(EdgeInsets())
                 CategoryRow(items: homeViewModel.categoriesInfo)
                     .listRowInsets(EdgeInsets())
