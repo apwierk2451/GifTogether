71d39ea37f3fd4f68c6b3d9c76b29e59b79dbd16
wongbingg <wb277@naver.com>
2023-03-14 16:08:15 +0900

    refactor : FirestoreRepository 인터페이스 수정 #4

--- GifTogether/GifTogether/Domain/Interface/FirestoreRepository.swift
+++ GifTogether/GifTogether/Domain/Interface/FirestoreRepository.swift
@@ -8,8 +8,10 @@
 import Foundation
 
 protocol FirestoreRepository {
-    func create(_ entity: Entity)
-    func read() -> [Entity]
-    func update(_ entity: Entity, to modifiedEntity: Entity)
-    func delete(_ entity: Entity)
+    associatedtype T: Entity
+    
+    func create(_ entity: T)
+    func read(_ completion: @escaping ([T]?) -> Void)
+    func update(documentId: String, to modifiedEntity: T)
+    func delete(documentId: String)
 }
