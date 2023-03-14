b7b48a6b5c2b12c900d935c4813539e2e77e4c1a
wongbingg <wb277@naver.com>
2023-03-14 16:13:03 +0900

    refactor : Gifticon 모델 수정 #4
    - Category 와 Brand 열거형 적용

--- GifTogether/GifTogether/Domain/Entities/Gifticon.swift
+++ GifTogether/GifTogether/Domain/Entities/Gifticon.swift
@@ -8,16 +8,62 @@
 import Foundation
 
 struct Gifticon: Entity {
+    static let Id: String = "gifticon"
+    
+    let uuid: String
     let name: String
     let codeNumber: String
     let brand: Brand
+    let category: Category
     let originalPrice: String
     let discountedPrice: String
     let imageURL: String
     let expirationDate: String
-    let provider: UserInfo
+    let providerUID: String
     
-    enum Brand {
+    func toDictionary() -> Dictionary<String, Any> {
+        return [
+            "uuid": uuid,
+            "name": name,
+            "codeNumber": codeNumber,
+            "brand": brand.rawValue,
+            "category": category.rawValue,
+            "originalPrice": originalPrice,
+            "discountedPrice": discountedPrice,
+            "imageURL": imageURL,
+            "expirationDate": expirationDate,
+            "providerUID": providerUID
+        ]
+    }
+    
+    static func toEntity(dic: [String : Any]) -> Gifticon {
+        guard let uuid = dic["uuid"] as? String,
+              let name = dic["name"] as? String,
+              let codeNumber = dic["codeNumber"] as? String,
+              let brand = dic["brand"] as? String,
+              let category = dic["category"] as? String,
+              let originalPrice = dic["originalPrice"] as? String,
+              let discountedPrice = dic["discountedPrice"] as? String,
+              let imageURL = dic["imageURL"] as? String,
+              let expirationDate = dic["expirationDate"] as? String,
+              let providerUID = dic["providerUID"] as? String else {
+            
+            return .stub()
+        }
+        
+        return Gifticon(uuid: uuid,
+                        name: name,
+                        codeNumber: codeNumber,
+                        brand: Brand(rawValue: brand)!,
+                        category: Category(rawValue: category)!,
+                        originalPrice: originalPrice,
+                        discountedPrice: discountedPrice,
+                        imageURL: imageURL,
+                        expirationDate: expirationDate,
+                        providerUID: providerUID)
+    }
+    
+    enum Brand: String {
         case starbucks
         case bbq
         
@@ -37,5 +83,37 @@ struct Gifticon: Entity {
                 return "비비큐 브랜드 이미지 url"
             }
         }
+    }
+    
+    enum Category: String {
+        case coffee
+        case food
+        case movie
+        case iceCream
+    }
+}
+
+extension Gifticon {
+    
+    static func stub(uuid: String = "",
+                     name: String = "",
+                     codeNumber: String = "",
+                     brand: Brand = .bbq,
+                     category: Category = .coffee,
+                     originalPrice: String = "",
+                     discountedPrice: String = "",
+                     imageURL: String = "",
+                     expirationDate: String = "",
+                     providerUID: String = "") -> Self {
+        .init(uuid: uuid,
+              name: name,
+              codeNumber: codeNumber,
+              brand: brand,
+              category: category,
+              originalPrice: originalPrice,
+              discountedPrice: discountedPrice,
+              imageURL: imageURL,
+              expirationDate: expirationDate,
+              providerUID: providerUID)
     }
 }
