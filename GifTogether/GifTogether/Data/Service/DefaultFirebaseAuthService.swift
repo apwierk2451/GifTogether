8afd2ed50cd6ff99fa0cfbcbc531e8b77fdfd9cc
wongbingg <wb277@naver.com>
2023-03-14 16:07:40 +0900

    feat : FirebaseAuthService 프로토콜 채택, 구현 #4

---
+++ GifTogether/GifTogether/Data/Service/DefaultFirebaseAuthService.swift
@@ -0,0 +1,50 @@
+//
+//  DefaultFirebaseAuthService.swift
+//  GifTogether
+//
+//  Created by 이원빈 on 2023/03/14.
+//
+
+import FirebaseAuth
+
+final class DefaultFirebaseAuthService: FirebaseAuthService {
+    
+    func createUser(email: String,
+                    password: String,
+                    _ completion: @escaping (String?) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { authDataResult, error in
+            guard error == nil else {
+                completion(nil)
+                return
+            }
+            
+            if let authDataResult = authDataResult {
+                completion(authDataResult.user.uid)
+            }
+        }
+    }
+    
+    func logIn(email: String,
+               password: String,
+               _ completion: @escaping (String?) -> Void) {
+        
+        Auth.auth().signIn(withEmail: email, password: password) { authDataResult, error in
+            guard error == nil else {
+                completion(nil)
+                return
+            }
+            
+            if let authDataResult = authDataResult {
+                completion(authDataResult.user.uid)
+            }
+        }
+    }
+    
+    func fetchUserUID() throws -> String {
+        if let userUID = Auth.auth().currentUser?.uid {
+            return userUID
+        } else {
+            throw FirebaseAuthServiceError.failToFetch
+        }
+    }
+}
