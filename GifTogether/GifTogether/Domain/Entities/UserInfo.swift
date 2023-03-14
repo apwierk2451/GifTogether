dd5a808182022bc5db6458d9956a7f85d72d48d7
wongbingg <wb277@naver.com>
2023-03-14 16:12:21 +0900

    refactor : UserInfo 모델 수정 #4
    - 계좌정보 제거

--- GifTogether/GifTogether/Domain/Entities/UserInfo.swift
+++ GifTogether/GifTogether/Domain/Entities/UserInfo.swift
@@ -8,18 +8,45 @@
 import Foundation
 
 struct UserInfo: Entity {
+    static let Id: String = "userInfo"
+    
+    let uuid: String
     let name: String
     let email: String
     let phoneNumber: String
-    let accountInfo: AccountInfo
     
-    struct AccountInfo {
-        let accountNumber: String
-        let bank: Bank
-        
-        enum Bank {
-            case nonghyub
-            case hana
+    func toDictionary() -> Dictionary<String, Any> {
+        return [
+            "name": name,
+            "email": email,
+            "phoneNumber": phoneNumber
+        ]
+    }
+    
+    static func toEntity(dic: [String : Any]) -> UserInfo {
+        guard let uuid = dic["uuid"] as? String,
+              let name = dic["name"] as? String,
+              let email = dic["email"] as? String,
+              let phoneNumber = dic["phoneNumber"] as? String else {
+            return .stub()
         }
+        
+        return UserInfo(uuid: uuid,
+                        name: name,
+                        email: email,
+                        phoneNumber: phoneNumber)
+    }
+}
+
+extension UserInfo {
+    
+    static func stub(uuid: String = "",
+                     name: String = "",
+                     email: String = "",
+                     phoneNumber: String = "") -> Self {
+        .init(uuid: uuid,
+              name: name,
+              email: email,
+              phoneNumber: phoneNumber)
     }
 }
