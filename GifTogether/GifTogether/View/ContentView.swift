8218c6363ec19cb0f760d6c060e2174e810e0fa9
wongbingg <wb277@naver.com>
2023-03-19 12:17:38 +0900

    refactor : "ContentView" 에서 LoginView 시작 #9

--- GifTogether/GifTogether/View/ContentView.swift
+++ GifTogether/GifTogether/View/ContentView.swift
@@ -8,43 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selection: Tab = .home
-    @StateObject private var homeViewModel = HomeViewModel()
-    
-    enum Tab {
-        case home
-        case search
-        case favorite
-        case profile
-    }
     
     var body: some View {
-        TabView(selection: $selection) {
-            HomeView()
-                .environmentObject(homeViewModel)
-                .tabItem {
-                    Image(systemName: "house")
-                }
-                .tag(Tab.home)
-            SearchView()
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                }
-                .tag(Tab.search)
-            
-            FavoriteView()
-                .tabItem {
-                    Image(systemName: "heart")
-
-                }
-                .tag(Tab.favorite)
-            
-            ProfileView()
-                .tabItem {
-                    Image(systemName: "person.crop.circle.fill")
-                }
-                .tag(Tab.profile)
-        }
+        LoginView()
     }
 }
 
