baf79f9691a7859d58ab5812001fa30ded77dfc7
wongbingg <wb277@naver.com>
2023-03-19 12:17:03 +0900

    feat : 기존의 ContentView 들을 MainView로 분리 #9
    - 탭바 4개를 가지는 TabView 를 MainView로 분리
    - 이유 : 로그인화면부터 시작해야 하기 때문

---
+++ GifTogether/GifTogether/View/MainView.swift
@@ -0,0 +1,61 @@
+//
+//  MainView.swift
+//  GifTogether
+//
+//  Created by 이원빈, 이은찬 on 2023/03/17.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @State private var selection: Tab = .home
+    @StateObject private var homeViewModel = DIContainer().makeHomeViewModel()
+    @StateObject private var myPageViewModel = DIContainer().makeMyPageViewModel()
+    
+    @Binding var logout: Bool
+    enum Tab {
+        case home
+        case search
+        case favorite
+        case profile
+    }
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            HomeView()
+                .environmentObject(homeViewModel)
+                .tabItem {
+                    Image(systemName: "house")
+                }
+                .tag(Tab.home)
+            SearchView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+                .tag(Tab.search)
+
+            FavoriteView()
+                .tabItem {
+                    Image(systemName: "heart")
+
+                }
+                .tag(Tab.favorite)
+
+            ProfileView(shouldShowMainView: $logout)
+                .environmentObject(myPageViewModel)
+                .tabItem {
+                    Image(systemName: "person.crop.circle.fill")
+                }
+                .tag(Tab.profile)
+        }
+        .onAppear {
+            myPageViewModel.fetchUserInfo()
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView(logout: .constant(true))
+    }
+}
