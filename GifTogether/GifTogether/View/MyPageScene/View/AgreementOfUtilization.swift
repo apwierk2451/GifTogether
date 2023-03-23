4883a3d0fe3b35c6871c372a31cacf744c1e42f3
wongbingg <wb277@naver.com>
2023-03-23 17:26:17 +0900

    feat : 이용약관과 개인정보 빈 뷰 생성 #13

---
+++ GifTogether/GifTogether/View/MyPageScene/View/AgreementOfUtilization.swift
@@ -0,0 +1,20 @@
+//
+//  AgreementOfUtilizationView.swift
+//  GifTogether
+//
+//  Created by 이원빈 on 2023/03/19.
+//
+
+import SwiftUI
+
+struct AgreementOfUtilization: View {
+    var body: some View {
+        Text("이용약관")
+    }
+}
+
+struct AgreementOfUtilizationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AgreementOfUtilization()
+    }
+}
---
+++ GifTogether/GifTogether/View/MyPageScene/View/PersonalInfomation.swift
@@ -0,0 +1,20 @@
+//
+//  PersonalInfomation.swift
+//  GifTogether
+//
+//  Created by 이원빈 on 2023/03/19.
+//
+
+import SwiftUI
+
+struct PersonalInfomation: View {
+    var body: some View {
+        Text("개인정보 처리방침")
+    }
+}
+
+struct PersonalInfomation_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonalInfomation()
+    }
+}
