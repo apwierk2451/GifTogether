147fabca74053eb11899ef3aec7e0a41dbfda8f3
wongbingg <wb277@naver.com>
2023-04-04 18:54:54 +0900

    feat : GifticonQuery 코어데이터 생성 #19

---
+++ GifTogether/GifticonQueryEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  GifticonQueryEntity+CoreDataClass.swift
+//  GifTogether
+//
+//  Created by 이원빈 on 2023/03/30.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(GifticonQueryEntity)
+public class GifticonQueryEntity: NSManagedObject {
+
+}
