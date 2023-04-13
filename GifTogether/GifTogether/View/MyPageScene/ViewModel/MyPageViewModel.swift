3a93a9982841b8f369d2a2cf49eff29c8f13594d
wongbingg <wb277@naver.com>
2023-04-13 16:18:32 +0900

    refactor : MyPageScene ViewModel 수정 #25
    - fetchGifticonUseCase 추가
    - salesList @Published 프로퍼티 추가

--- GifTogether/GifTogether/View/MyPageScene/ViewModel/MyPageViewModel.swift
+++ GifTogether/GifTogether/View/MyPageScene/ViewModel/MyPageViewModel.swift
@@ -9,24 +9,43 @@ import Combine
 
 final class MyPageViewModel: ObservableObject {
     private let fetchUserInfoUseCase: FetchUseInfoUseCase
+    private let fetchGifticonUseCase: FetchGifticonUseCase
     private let deleteAccountUseCase: DeleteAccountUseCase
     
     @Published var userInfo: UserInfo = .stub()
+    @Published var salesList: [Gifticon] = []
     
     init(
         fetchUserInfoUseCase: FetchUseInfoUseCase,
+        fetchGifticonUseCase: FetchGifticonUseCase,
         deleteAccountUseCase: DeleteAccountUseCase
     ) {
         self.fetchUserInfoUseCase = fetchUserInfoUseCase
+        self.fetchGifticonUseCase = fetchGifticonUseCase
         self.deleteAccountUseCase = deleteAccountUseCase
+    }
+    
+    private func fetchSalesList(with uuidList: [String]) {
+        guard uuidList.isEmpty == false else { return }
+        for uuid in uuidList {
+            fetchGifticonUseCase.execute(uuid: uuid) { [weak self] gifticon in
+                guard let gifticon = gifticon,
+                        self?.salesList.contains(where: { element in
+                    element.uuid == gifticon.uuid }) == false else { return }
+                self?.salesList.append(gifticon)
+            }
+        }
     }
     
     func fetchUserInfo() {
         guard let userUID = UserDefaults.standard.object(forKey: "userUID") as? String else {
             return
         }
-        fetchUserInfoUseCase.execute(with: userUID) { userInfo in
-            self.userInfo = userInfo
+        fetchUserInfoUseCase.execute(with: userUID) { [weak self] userInfo in
+            if let userInfo = userInfo {
+                self?.userInfo = userInfo
+                self?.fetchSalesList(with: userInfo.salesList)
+            }
         }
     }
     
