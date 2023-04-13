6b3dc91216d0fa6baaf8ca1ced6d4f9b6344f4bc
wongbingg <wb277@naver.com>
2023-04-13 16:34:55 +0900

    refactor : HomeView 에 기프티콘 등록 플로팅버튼 추가 #25

--- GifTogether/GifTogether/View/HomeScene/View/HomeView/HomeView.swift
+++ GifTogether/GifTogether/View/HomeScene/View/HomeView/HomeView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct HomeView: View {
     @EnvironmentObject var homeViewModel: HomeViewModel
+    @State var showHiddenMenu: Bool = false
+    @State var showModal: Bool = false
     
     var body: some View {
-        NavigationView {
+        ZStack {
             List {
                 BannerView()
                     .listRowInsets(EdgeInsets())
@@ -22,16 +24,62 @@ struct HomeView: View {
                     categoryName: "추천상품",
                     items: homeViewModel.gifticonsInfo
                 )
-                .navigationTitle("GifTogether")
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationBarItems(trailing: Image(systemName: "bell"))
-                .navigationBarBackButtonHidden()
+                .padding(.top)
+                .listRowInsets(EdgeInsets())
             }
-            .listRowInsets(EdgeInsets())
             .listStyle(.inset)
-        }
-        .task {
-            homeViewModel.request()
+            .navigationTitle("GifTogether")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(trailing: Image(systemName: "bell"))
+            .navigationBarBackButtonHidden()
+            .task {
+                homeViewModel.request()
+            }
+
+            Color(uiColor: .black).opacity(showHiddenMenu ? 0.5 : 0.0)
+                .animation(Animation.easeInOut(duration: 0.3))
+                .onTapGesture {
+                    showHiddenMenu.toggle()
+                }
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    ZStack {
+                        Text("내 기프티콘 판매")
+                            .font(.body)
+                            .foregroundColor(.white)
+                            .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+                            .background(Color(uiColor: .orange).cornerRadius(8))
+                            .offset(x: showHiddenMenu ? -20 : 0, y: showHiddenMenu ? -60 : 0)
+                            .scaleEffect(showHiddenMenu ? 1.0 : 0)
+                            .animation(.ripple(), value: showHiddenMenu)
+                            .sheet(isPresented: $showModal) {
+                                RegisterView(shouldPresent: $showModal)
+                            }
+                            .onTapGesture {
+                                showModal = true
+                                showHiddenMenu = false
+                            }
+                        
+                        Image(systemName: showHiddenMenu ? "xmark.circle.fill" : "plus.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .scaleEffect(showHiddenMenu ? 0.8 : 1.0)
+                            .animation(.ripple(), value: showHiddenMenu)
+                            .foregroundColor(showHiddenMenu ? .secondary : .orange)
+                            .background(Color(uiColor: .white).cornerRadius(25).scaleEffect(0.8))
+                            .padding()
+                            .padding(.leading, 40)
+                            .onTapGesture {
+                                showHiddenMenu.toggle()
+                            }
+                    }
+                }
+            }
         }
     }
 }
