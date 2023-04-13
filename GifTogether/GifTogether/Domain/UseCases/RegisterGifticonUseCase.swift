00322d41dee17e804bc2d0cb69a54ae3c1c08b1b
wongbingg <wb277@naver.com>
2023-04-13 15:05:37 +0900

    refactor : RegisterGifticonUseCase 수정 #25
    - UseCase 내부에서 salesList 업데이트

--- GifTogether/GifTogether/Domain/UseCases/RegisterGifticonUseCase.swift
+++ GifTogether/GifTogether/Domain/UseCases/RegisterGifticonUseCase.swift
@@ -12,9 +12,19 @@ protocol RegisterGifticonUseCase {
 }
 
 struct DefaultRegisterGifticonUseCase: RegisterGifticonUseCase {
+    let userInfoRepository: UserInfoRepository
     let gifticonRepository: GifticonRepository
     
     func execute(with item: Gifticon) {
         gifticonRepository.create(item)
+        guard let userUID = UserDefaults.standard.string(forKey: "userUID") else {
+            return
+        }
+        userInfoRepository.read { userInfoList in
+            guard let userInfoList = userInfoList else { return }
+            var fetchedUserInfo = userInfoList.filter { $0.uuid == userUID }[0]
+            fetchedUserInfo.salesList.append(item.uuid)
+            userInfoRepository.update(documentId: userUID, to: fetchedUserInfo)
+        }
     }
 }
