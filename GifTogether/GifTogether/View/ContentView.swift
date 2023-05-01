4270673dad79526aaff9e86bcda1d29b2262c77e
wongbingg <wb277@naver.com>
2023-05-01 20:16:18 +0900

    refactor : 오류메세지로 인해 UIApplication Extension 파일 제거 #9
    - 다른방법으로 키보드 숨기기 해결

--- GifTogether/GifTogether/View/ContentView.swift
+++ GifTogether/GifTogether/View/ContentView.swift
@@ -11,8 +11,8 @@ struct ContentView: View {
     
     var body: some View {
         LoginView()
-            .onAppear {
-                UIApplication.shared.hideKeyboard()
+            .onTapGesture {
+                hideKeyboard()
             }
     }
 }
